aee626668b97d9233caf6671923e1d1ab20e20b7
Kia Farhang <kfarhang0@gmail.com>
2017-12-16 09:53:49 -0600

    Project setup: typescript, webpack

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// Find the header
+// if header doesn't exist, return
+// Create div within div; append to header
+
+
+//Listen for (throttled) scroll events:
+
+//Check how far down the page user has scrolled
+//Convert to percentage
+//Use percentage to set inner progress div's width
+
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'wide-template-progress-bar.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                }
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    // http://vue-loader.vuejs.org/en/workflow/production.html
+    module.exports.plugins = (module.exports.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: false,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ])
+}
